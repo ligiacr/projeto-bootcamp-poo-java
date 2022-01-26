edcff89b8f73d02799953cd3e1a36d4cf03d32c7
ligiacr <ligia_c_rodrigues@hotmail.com>
2022-01-25 22:45:11 -0300

    Cria novas classes

--- src/me/dio/projeto/dominio/Main.java
+++ src/me/dio/projeto/dominio/Main.java
@@ -20,8 +20,40 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição: Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLigia = new Dev();
+        devLigia.setNome("Lígia");
+        devLigia.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos - Lígia: " + devLigia.getConteudosInscritos());
+        devLigia.progredir();
+        devLigia.progredir();
+        devLigia.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos - Lígia: " + devLigia.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos - Lígia: " + devLigia.getConteudosConcluidos());
+        System.out.println("XP: " + devLigia.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devJose = new Dev();
+        devJose.setNome("José");
+        devJose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos - José: " + devJose.getConteudosInscritos());
+        devJose.progredir();
+        devJose.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos - José: " + devJose.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos - José: " + devJose.getConteudosConcluidos());
+        System.out.println("XP: " + devJose.calcularTotalXp());
     }
 }
